24a1a13b461c6a03064d0f1282ccfdf5790a337a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-02 13:48:39 -0700

    add placeholder function - for planning app staticly

---
+++ src/components/NewKegForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewKegForm(){
+  return (
+    <React.Fragment>
+      <h1>NewKegForm Placeholder</h1>
+    </React.Fragment>
+  );
+}
+
+export default NewKegForm;
