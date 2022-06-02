7dbe0f691d9ce10c4abfa58b106013f09d5d5c29
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-02 16:25:39 -0700

    removed placeholder text with keg name prop

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -4,9 +4,8 @@ import PropTypes from "prop-types";
 function Keg(props){
   return (
     <React.Fragment>
-      <h1>Keg Placeholder</h1>
+      <h1>{props.name}</h1>
       <hr/>
-      <h3>{props.name}</h3>
       <h3>{props.brand}</h3>
       <h3>{props.flavor}</h3>
       <h3>{props.price}</h3>
