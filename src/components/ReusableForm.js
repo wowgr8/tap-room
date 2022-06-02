c78a94d87f2f0ee8168d745b4b245ed155b7141a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-02 13:48:45 -0700

    add placeholder function - for planning app staticly

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ReusableForm(){
+  return (
+    <React.Fragment>
+      <h1>ReusableForm Placeholder</h1>
+    </React.Fragment>
+  );
+}
+
+export default ReusableForm;
