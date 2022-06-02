c0d12a71935146899025c8682c26fd6478602208
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-02 14:51:20 -0700

    add class component - prepped for holding all state, WIP

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -1,11 +1,28 @@
 import React from "react";
 
-function KegControl(){
-  return (
-    <React.Fragment>
-      <h1>KegControl Placeholder: Will house State and all other components.</h1>
-    </React.Fragment>
-  );
+class KegControl extends React.Component{
+
+  //Constructor w/state
+
+
+
+  // Methods for buttons
+
+
+
+  render(){
+    // conditional rendering
+
+
+
+
+
+    return (
+      <React.Fragment>
+        <h1>KegControl Placeholder: Will house State and all other components.</h1>
+      </React.Fragment>
+    );
+  }
 }
 
 export default KegControl;
