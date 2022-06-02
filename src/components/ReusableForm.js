03128448392a208cfca480aeb011f5833fde9762
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-02 16:53:18 -0700

    add reusable form, the onSubmit is now set to prop called formSubmissionHandler so form for editing will trigger different method than form creating new keg. Also passed in prop for buttonText for the same reason

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,11 +1,37 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function ReusableForm(){
+function ReusableForm(props){
   return (
     <React.Fragment>
-      <h1>ReusableForm Placeholder</h1>
+      <form className="block col-2" onSubmit = {props.formSubmissionHandler}>
+        <div className="row">
+            <input
+              type='text'
+              name='name'
+              placeholder='Kombucha Name' />
+            <input
+              type='text'
+              name='brand'
+              placeholder='Kombucha brand' />
+            <input
+              type='text'
+              name='flavor'
+              placeholder='Kombucha flavor' />
+            <input
+              type='number'
+              name='price'
+              placeholder='Kombucha price' />
+            <button type='submit'>{props.buttonText}</button>
+        </div>
+      </form> 
     </React.Fragment>
   );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
 }
 
 export default ReusableForm;
