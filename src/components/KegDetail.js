ac19fe10db278d41781e50fc5670e133caf5eccf
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-02 13:48:27 -0700

    add placeholder function - for planning app staticly

---
+++ src/components/KegDetail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function KegDetail(){
+  return (
+    <React.Fragment>
+      <h1>KegDetail Placeholder</h1>
+    </React.Fragment>
+  );
+}
+
+export default KegDetail;
