f9207c81b17851d53fbf9c6ccff881e596a2e9cb
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-02 13:42:35 -0700

    add placeholder function - for planning app staticly

---
+++ src/components/KegControl.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function KegControl(){
+  return (
+    <React.Fragment>
+      <h1>KegControl Placeholder: Will house State and all other components.</h1>
+    </React.Fragment>
+  );
+}
+
+export default KegControl;
