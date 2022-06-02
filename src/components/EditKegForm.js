b2dbb2f30b59ce3e4c90b81035247f3a9cce6268
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-02 13:48:15 -0700

    add placeholder function - for planning app staticly

---
+++ src/components/EditKegForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EditKegForm(){
+  return (
+    <React.Fragment>
+      <h1>EditKegForm Placeholder</h1>
+    </React.Fragment>
+  );
+}
+
+export default EditKegForm;
