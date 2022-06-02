9896dd1497a47cfb1413d4eaea03a219941ae244
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-02 15:47:52 -0700

    added proptypes and general structure for view once returned

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,11 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Keg(){
+function Keg(props){
   return (
     <React.Fragment>
       <h1>Keg Placeholder</h1>
+      <hr/>
+      <h3>{props.name}</h3>
+      <h3>{props.brand}</h3>
+      <h3>{props.flavor}</h3>
+      <h3>{props.price}</h3>
     </React.Fragment>
   );
+}
+
+Keg.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  flavor: PropTypes.string,
+  price: PropTypes.number
 }
 
 export default Keg;
