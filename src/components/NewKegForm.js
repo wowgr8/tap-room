e7e584729b020d727c3ce79608ba5e98d56534fa
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-02 16:29:36 -0700

    add uuid and proptypes import. Added function called handleNewKegSubmission which collects data inputted into form and passes them back to control via onNewKegCreation callback. Lastly added form with new function attached to onSubmit event handler

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,16 +1,52 @@
 import React from "react";
+import { v4 } from "uuid";
+import PropTypes from "prop-types";
 
-function NewKegForm(){
+function NewKegForm(props){
+
+  function handleNewKegSubmission(event){
+    // function for eventhandler will go here (it's being passed down from control)
+    event.preventDefault();
+    props.onNewKegCreation({ name: event.target.name.value,
+                              brand: event.target.brand.value,
+                              flavor: event.target.flavor.value,
+                              price: event.target.price.value,
+                              id: v4()
+                            })
+  }
+
   return (
     <React.Fragment>
       <newkegform className="block col-2">
-        <h2>NewKegForm Placeholder</h2>
+        <h2>Add New Keg</h2>
         <div className="row">
-          <h2>ReusableForm placeholder</h2>
+          <form onSubmit={handleNewKegSubmission}>
+            <input
+              type='text'
+              name='name'
+              placeholder='Kombucha Name' />
+            <input
+              type='text'
+              name='brand'
+              placeholder='Kombucha brand' />
+            <input
+              type='text'
+              name='flavor'
+              placeholder='Kombucha flavor' />
+            <input
+              type='number'
+              name='price'
+              placeholder='Kombucha price' />
+            <button type='submit'>Add Keg!</button>
+          </form>
         </div>
       </newkegform>   
     </React.Fragment>
   );
 }
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
 
 export default NewKegForm;
