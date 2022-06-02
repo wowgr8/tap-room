807db542a6acfeff19817be6931dd38cfbe14b72
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-02 16:00:48 -0700

    added props and keg import. Added .map() to loop keg's.

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,16 +1,30 @@
 import React from "react";
+import Keg from "./Keg";
+import PropTypes from "prop-types";
 
-function KegList(){
+function KegList(props){
   return (
     <React.Fragment>
       <keglist className="block col-2">
         <h2>KegList Placeholder</h2>
         <div className="row">
-          <h2>Keg placeholder</h2>
+          <hr/>
+          {props.kegList.map((keg, index) =>
+            <Keg
+              name = {keg.name}
+              brand = {keg.brand}
+              flavor = {keg.flavor}
+              price = {keg.price}
+              key = {index} />
+            )}
         </div>
       </keglist>      
     </React.Fragment>
   );
 }
+
+KegList.propTypes = {
+  kegList: PropTypes.array
+};
 
 export default KegList;
