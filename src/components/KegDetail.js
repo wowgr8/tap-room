f091f5ff1afc45fae26a2b5b718825d7f87de62c
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-03 18:38:19 -0700

    add import statement for proptypes, define keg as object proptype and used object destructuring to return keg object properties

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -1,11 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function KegDetail(){
+function KegDetail(props){
+  const { keg } = props;
+
   return (
     <React.Fragment>
-      <h1>KegDetail Placeholder</h1>
+      <h1>Keg Detail </h1>
+      <h3>{keg.name}</h3>
+      <h3>{keg.brand}</h3>
+      <h3>{keg.flavor}</h3>
+      <h3>{keg.price}</h3>
+      <hr/>
     </React.Fragment>
   );
 }
+
+KegDetail.propTypes = {
+  keg: PropTypes.object
+};
 
 export default KegDetail;
