3e71a5bb2dee5e9e69b6eeed8c404f34d898a522
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-03 20:16:28 -0700

    add import statement for proptypes. Add new function that grabs values from the form and then triggers handlEditingKegInList(via onEditKeg prop being passed down)

--- src/components/EditKegForm.js
+++ src/components/EditKegForm.js
@@ -1,14 +1,32 @@
 import React from "react";
 import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
 
-function EditKegForm(){
+function EditKegForm(props){
+  const { keg } = props;
+
+  function handleEditKegFormSubmission(event){
+    event.preventDefault();
+    props.onEditKeg({name: event.target.name.value,
+                    brand: event.target.brand.value,
+                    flavor: event.target.flavor.value,
+                    price: event.target.price.value,
+                    id: keg.id});
+  }
+
   return (
     <React.Fragment>
       <h1>EditKegForm Placeholder</h1>
       <ReusableForm
+        formSubmissionHandler = {handleEditKegFormSubmission}
         buttonText = "Update Keg"  />
     </React.Fragment>
   );
 }
+
+EditKegForm.propTypes = {
+  keg: PropTypes.object,
+  onEditKeg: PropTypes.func
+};
 
 export default EditKegForm;
