f5805bf8ca0a3d642d8c46eebadad6aabab2846a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-03 18:03:47 -0700

    add selectedKeg state slice set to null. Add method to handle click event on keg to see detail, this method will be passed down through KegList component tag as prop. and Added conditional for rendering KegDetail conponent when selectedKeg is not null. Also, added import for KegDetail component

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NewKegForm from "./NewKegForm";
 import KegList from "./KegList";
+import KegDetail from "./KegDetail";
 
 class KegControl extends React.Component{
 
@@ -9,7 +10,8 @@ class KegControl extends React.Component{
     super(props);
     this.state = {
       formVisibleOnPage: false,
-      mainKegList: []
+      mainKegList: [],
+      selectedKeg: null
     };
   }
 
@@ -27,16 +29,24 @@ class KegControl extends React.Component{
                   formVisibleOnPage: false});
   }
 
+  // This method will take the id property of a keg, find the correct keg, and then set selectedkeg equal to that keg. 
+  handleChangingSelectedKeg = (id) => {
+    const selectedKeg = this.state.mainKegList.filter(keg => keg.id === id)[0];
+    this.setState({selectedKeg: selectedKeg});
+  }
 
   render(){
     // conditional rendering
     let currentlyVisibleState = null;
     let buttonText = null;
-    if(this.state.formVisibleOnPage){
+    if(this.state.selectedKeg != null){
+      currentlyVisibleState = <KegDetail keg = {this.state.selectedKeg} />
+      buttonText = "Return to Keg List";
+    } else if(this.state.formVisibleOnPage){
       currentlyVisibleState = <NewKegForm onNewKegCreation = {this.handleAddingNewKegToList}/>
       buttonText = "Return to Keg List";
     } else {
-      currentlyVisibleState = <KegList kegList = {this.state.mainKegList}/>
+      currentlyVisibleState = <KegList kegList = {this.state.mainKegList} onKegSelection = {this.handleChangingSelectedKeg} />
       buttonText = "Add New Keg";
     }
 
