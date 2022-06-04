7dbc440f293bc6a6530526a8cace2457bfc5e7c0
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-04 15:37:21 -0700

    add working KegRefill/onAdd button - issue with conditional -WIP

--- src/components/KegStock.js
+++ src/components/KegStock.js
@@ -1,14 +1,20 @@
 import React from "react";
 
 function KegStock(props){
+  const {kegQty, onAdd} = props;
+
   return (
     <React.Fragment>
       <p>Card showing pint and keg quantity placeholder</p>
       <h3>will also show two buttons, to add a keg(124pints) and sell a pint</h3>
       <h3>Conditional</h3>
       <div className="row">
-          {/* <button onClick={ props.onClickingEdit }>Refill Keg</button> */}
-          {/* <button onClick = {() => onClickingDelete(keg.id)}>Sell Pint</button> */}
+        ({kegQty === 0}){
+          <h3> 0 : Out of Stock. Please refill keg </h3>
+        } else {
+          <h3>Pints: {kegQty} </h3>
+        }
+          <button onClick={ onAdd}>Tap A New Keg</button>
       </div>
     </React.Fragment>
   );
