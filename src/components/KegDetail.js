a9cc105d507ccd884d34d09b09df48a0d2aac7b4
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-03 19:42:55 -0700

    add onClickingEdit prop to proptypes and update button

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function KegDetail(props){
-  const { keg, onClickingDelete } = props;
+  const { keg, onClickingDelete, onClickingEdit } = props;  // may need to remove deconstructed onClickingEdit then replace line 15 w/ <button onClick={ props.onClickingEdit }>Update Ticket</button>
 
   return (
     <React.Fragment>
@@ -10,16 +10,18 @@ function KegDetail(props){
       <h3>{keg.name}</h3>
       <h3>{keg.brand}</h3>
       <h3>{keg.flavor}</h3>
-      <h3>{keg.price}</h3>
-      <button onClick = {() => onClickingDelete(keg.id)}>Delete Keg</button>
+      <h3>{keg.price}</h3>     
       <hr/>
+      <button onClick = {() => onClickingEdit(keg.id)}>Update Keg</button> 
+      <button onClick = {() => onClickingDelete(keg.id)}>Delete Keg</button>
     </React.Fragment>
   );
 }
 
 KegDetail.propTypes = {
   keg: PropTypes.object,
-  onClickingDelete: PropTypes.func
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
 };
 
 export default KegDetail;
