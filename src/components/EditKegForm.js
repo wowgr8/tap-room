82d6c9df344b1e4dfcf3ec0ffab3e24a228f1446
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-03 19:55:21 -0700

    add reusable form and import, also added buttonText for upcoming method that handles applying update - WIP

--- src/components/EditKegForm.js
+++ src/components/EditKegForm.js
@@ -1,9 +1,12 @@
 import React from "react";
+import ReusableForm from "./ReusableForm";
 
 function EditKegForm(){
   return (
     <React.Fragment>
       <h1>EditKegForm Placeholder</h1>
+      <ReusableForm
+        buttonText = "Update Keg"  />
     </React.Fragment>
   );
 }
