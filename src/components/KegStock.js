812aa6d20cc147e374a1c4963b935af0c9e97f89
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-04 19:42:19 -0700

    add working onSell function for selling pints, and not allowing pintCount to sell below 0. Removed placeholder text and comments

--- src/components/KegStock.js
+++ src/components/KegStock.js
@@ -1,47 +1,34 @@
 import React from "react";
 
 function KegStock(props){
-  const {kegQty, setKegQty, buttonText, newbuttonText} = props;
+  const {kegQty, setKegQty, buttonText, newButtonText, sellButtonText, newSellButtonText} = props;
 
   const onAdd = () => {    
-    if(kegQty !== 0){
+    if(kegQty >= 0){
       setKegQty(prevKegQty => prevKegQty + 124);
-      newbuttonText(prevButtonText => prevButtonText.replace("Tap Another Keg"));
-      return buttonText
-    } else if(kegQty === 0){
-      console.log("No Stock")
-      newbuttonText(prevButtonText => prevButtonText.replace("Out of Stock - Pls Refill"));
-      setKegQty(prevKegQty => prevKegQty + 124);
+      newButtonText("Tap Another Keg");
+      newSellButtonText("Sell Pint");
+    } 
+  }
+
+  const onSell = () => {    
+    if(kegQty >= 1){
+      setKegQty(prevKegQty => prevKegQty - 1);
+      newSellButtonText("Sell Pint");
+    } else {
+      console.log("please refill keg")
+      newSellButtonText("Out of Stock.");
     }
   }
 
-
-    // playing with display messages in conditional
-  // const onAdd = () => {
-  //   let message = "";
-  //   if(kegQty !== 0){
-  //     setKegQty(prevKegQty => prevKegQty + 124);
-  //     let message = "Pints: " + kegQty;
-  //     return message,
-  //     console.log("Pints" + kegQty),
-  //     console.log(message)
-  //   } else if(kegQty === 0){
-  //     console.log("No Stock")
-  //     let message = <p>Out of Stock.</p>;
-  //     return message,
-  //     // console.log(kegQty, kegQty),
-  //     setKegQty(prevKegQty => prevKegQty + 124);
-  //   }
-  // }
-
   return (
     <React.Fragment>
-      <p>Card showing pint and keg quantity placeholder</p>
-      <h3>will also show two buttons, to add a keg(124pints) and sell a pint</h3>
-      <h3>Conditional</h3>
-      <div className="row">
-          <p>stock: {kegQty} pints</p>
+      <div className = "row">
+        <p>stock: {kegQty} pints</p>
+      </div>
+      <div className="row">          
           <button onClick={ onAdd}>{buttonText}</button>
+          <button onClick={ onSell}>{sellButtonText}</button>
       </div>
     </React.Fragment>
   );
