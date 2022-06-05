919a687403068dd4f934fa1ab88e9224b16a17f2
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-04 17:45:49 -0700

    added buttonText props to deconstructed object, removed broken conditional from return() and moved it into onAdd function which also updates pint stock and buttontext

--- src/components/KegStock.js
+++ src/components/KegStock.js
@@ -1,7 +1,38 @@
 import React from "react";
 
 function KegStock(props){
-  const {kegQty, onAdd} = props;
+  const {kegQty, setKegQty, buttonText, newbuttonText} = props;
+
+  const onAdd = () => {    
+    if(kegQty !== 0){
+      setKegQty(prevKegQty => prevKegQty + 124);
+      newbuttonText(prevButtonText => prevButtonText.replace("Tap Another Keg"));
+      return buttonText
+    } else if(kegQty === 0){
+      console.log("No Stock")
+      newbuttonText(prevButtonText => prevButtonText.replace("Out of Stock - Pls Refill"));
+      setKegQty(prevKegQty => prevKegQty + 124);
+    }
+  }
+
+
+    // playing with display messages in conditional
+  // const onAdd = () => {
+  //   let message = "";
+  //   if(kegQty !== 0){
+  //     setKegQty(prevKegQty => prevKegQty + 124);
+  //     let message = "Pints: " + kegQty;
+  //     return message,
+  //     console.log("Pints" + kegQty),
+  //     console.log(message)
+  //   } else if(kegQty === 0){
+  //     console.log("No Stock")
+  //     let message = <p>Out of Stock.</p>;
+  //     return message,
+  //     // console.log(kegQty, kegQty),
+  //     setKegQty(prevKegQty => prevKegQty + 124);
+  //   }
+  // }
 
   return (
     <React.Fragment>
@@ -9,12 +40,8 @@ function KegStock(props){
       <h3>will also show two buttons, to add a keg(124pints) and sell a pint</h3>
       <h3>Conditional</h3>
       <div className="row">
-        ({kegQty === 0}){
-          <h3> 0 : Out of Stock. Please refill keg </h3>
-        } else {
-          <h3>Pints: {kegQty} </h3>
-        }
-          <button onClick={ onAdd}>Tap A New Keg</button>
+          <p>stock: {kegQty} pints</p>
+          <button onClick={ onAdd}>{buttonText}</button>
       </div>
     </React.Fragment>
   );
